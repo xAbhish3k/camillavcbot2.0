a9762125e672c937f232b613e9932e2b43049504
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-08-29 15:11:12 +0530

    Update gcast.py

--- handlers/gcast.py
+++ handlers/gcast.py
@@ -7,7 +7,7 @@ import aiohttp
 import regex
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
-from VCPlayBot.config import SUDO_USERS, BOT_TOKEN
+from camillavcbot2.0.config import SUDO_USERS, BOT_TOKEN
 from aiohttp import ClientSession
 from pyrogram.types import Message
 

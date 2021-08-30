55691c21f8ebf85fc2e2627fc81a2ea6f08ecb2d
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-08-30 17:53:22 +0530

    Create incredible.py

---
+++ handlers/incredible.py
@@ -0,0 +1,64 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+
+@Client.on_message(
+    filters.command("help")
+    & filters.private
+    & ~ filters.edited
+)
+async def help_(client: Client, message: Message):
+    await message.reply_text(
+        f"""❤ The commands and there use is explained here by 𝓓𝓐𝓡𝓚𝓧𝓜𝓤𝓢𝓘𝓒 ❤
+**For all in group**
+- `/play <song name>` - play song you requested
+- `/dplay <song name>` - play song you requested via deezer
+- `/splay <song name>` - play song you requested via jio saavn
+- `/playlist` - Show now playing list
+- `/current` - Show now playing
+- `/song <song name>` - download songs you want quickly
+- `/search <query>` - search videos on youtube with details
+- `/deezer <song name>` - download songs you want quickly via deezer
+- `/saavn <song name>` - download songs you want quickly via saavn
+- `/video <song name>` - download videos you want quickly
+
+**Admins only**
+- `/player` - open music player settings panel
+- `/pause` - pause song play
+- `/resume` - resume song play
+- `/skip` - play next song
+- `/end` - stop music play
+- `/userbotjoin` - invite assistant to your chat
+- `/userbotleave` - remove assistant from your chat
+- `/admincache` - Refresh admin list""")
+
+@Client.on_message(
+    filters.command("help")
+    & filters.group
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""❤ The commands and there use is explained here by 𝓓𝓐𝓡𝓚𝓧𝓜𝓤𝓢𝓘𝓒 ❤
+**For all in group**
+- `/play <song name>` - play song you requested
+- `/dplay <song name>` - play song you requested via deezer
+- `/splay <song name>` - play song you requested via jio saavn
+- `/playlist` - Show now playing list
+- `/current` - Show now playing
+- `/song <song name>` - download songs you want quickly
+- `/search <query>` - search videos on youtube with details
+- `/deezer <song name>` - download songs you want quickly via deezer
+- `/saavn <song name>` - download songs you want quickly via saavn
+- `/video <song name>` - download videos you want quickly
+
+**Admins only**
+- `/player` - open music player settings panel
+- `/pause` - pause song play
+- `/resume` - resume song play
+- `/skip` - play next song
+- `/end` - stop music play
+- `/userbotjoin` - invite assistant to your chat
+- `/userbotleave` - remove assistant from your chat
+- `/admincache` - Refresh admin list""")
